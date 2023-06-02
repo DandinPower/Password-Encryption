9c72406099e9aa8e623fe6279e8a6ae11d46a0bf
DandinPower <tomhot246@gmail.com>
2023-06-02 21:26:40 +0800

    init

---
+++ main.py
@@ -0,0 +1,77 @@
+import hashlib
+import sys
+import csv
+import os
+from base64 import b64encode, b64decode
+from Cryptodome.Cipher import AES
+from Cryptodome.Random import get_random_bytes
+from dotenv import load_dotenv
+load_dotenv()
+
+SALT = b64decode(os.getenv('SALT'))
+
+# SALT = get_random_bytes(AES.block_size)
+def encrypt(plain_text, password):
+    private_key = hashlib.scrypt(
+        password.encode(), salt=SALT, n=2**14, r=8, p=1, dklen=32)
+    cipher_config = AES.new(private_key, AES.MODE_GCM, nonce=SALT)
+    cipher_text= cipher_config.encrypt(bytes(plain_text, 'utf-8'))
+    return b64encode(cipher_text).decode('utf-8')
+
+def decrypt(cipher_text, password):
+    cipher_text = b64decode(cipher_text)
+    private_key = hashlib.scrypt(
+        password.encode(), salt=SALT, n=2**14, r=8, p=1, dklen=32)
+    cipher = AES.new(private_key, AES.MODE_GCM, nonce=SALT)
+    decrypted = cipher.decrypt(cipher_text)
+    return decrypted
+
+def encrypt_to_bin(plain_text, password, output_file):
+    encrypted = encrypt(plain_text, password)
+    with open(output_file, 'wb') as file:
+        file.write(b64decode(encrypted))
+
+def decrypt_from_bin(input_file, password):
+    with open(input_file, 'rb') as file:
+        cipher_text = file.read()
+    cipher_text = b64encode(cipher_text)
+    decrypted = decrypt(cipher_text, password)
+    return bytes.decode(decrypted)
+
+def read_csv_to_plaintext(file_path):
+    plaintext = ""
+    with open(file_path, 'r', newline='', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            plaintext += ', '.join(row) + '\n'
+    return plaintext
+
+def write_plaintext_to_csv(plaintext, file_path):
+    rows = plaintext.split('\n')  # Split plaintext into rows
+    with open(file_path, 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        for row in rows:
+            columns = row.split(', ')  # Split each row into columns
+            writer.writerow(columns)
+
+def main():
+    arguments = sys.argv[1:]  # Exclude the script name
+    operation = arguments[0]
+    if operation == "encrypt":
+        operation, password, input_file, output_file = arguments
+        plainText = read_csv_to_plaintext(input_file)
+        encrypt_to_bin(plainText, password, output_file)
+    elif operation == "decrypt":
+        operation, password, input_file, output_file = arguments
+        try:
+            decrypted = decrypt_from_bin(input_file, password)
+            write_plaintext_to_csv(decrypted, output_file) 
+        except:
+            print("Wrong password")
+    elif operation == "get_salt":
+        salt = get_random_bytes(AES.block_size)
+        salt = b64encode(salt).decode('utf-8')
+        print(f"Your random salt is: {salt}")
+    else:
+        print('Unknown operation')
+main()
---
+++ readme.md
@@ -0,0 +1,81 @@
+# Password Manager
+
+This is a password manager program that allows you to encrypt and decrypt your passwords using the AES encryption algorithm.
+
+## Requirements
+
+- Python 3.6 or higher
+- Python packages: `pycryptodomex`, `pycryptodomex`, `python-dotenv`
+
+## Installation
+
+1. Clone the repository:
+
+```shell
+git clone https://github.com/DandinPower/Password-Encryption.git
+```
+
+2. Install the required Python packages:
+
+```shell
+pip install -r requirements.txt
+```
+
+## Usage
+
+To run the program, execute the following command:
+
+```shell
+python main.py <operation> <password> <input_file> <output_file>
+```
+
+- `<operation>`: Specify the operation to perform. Use `encrypt` to encrypt a CSV file or `decrypt` to decrypt a binary file. Use `get_salt` to generate a new salt.
+- `<password>`: The password to use for encryption or decryption.
+- `<input_file>`: The path to the input file (CSV file for encryption or binary file for decryption).
+- `<output_file>`: The path to the output file (binary file for encryption or CSV file for decryption).
+
+For example, to encrypt a CSV file:
+
+```shell
+python main.py encrypt mypassword input.csv encrypted.bin
+```
+
+To decrypt a binary file:
+
+```shell
+python main.py decrypt mypassword encrypted.bin output.csv
+```
+
+To generate a new salt and add it to the .env file:
+
+```shell
+python main.py get_salt
+```
+
+**Note**: After generating a new salt, add it to the .env file as the value for the `SALT` variable. Please make sure to replace `<new_salt_value>` with the actual generated salt value obtained from running `python main.py get_salt`.
+
+## .env Configuration
+
+To use the program, create a `.env` file in the project directory and add the following configuration:
+
+```
+SALT=<new_salt_value>
+```
+
+Replace `<new_salt_value>` with the generated salt value obtained from running `python main.py get_salt`.
+
+## License
+
+This project is licensed under the MIT License. See the `LICENSE` file for more details.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions, feel free to open an issue or submit a pull request.
+
+## Acknowledgements
+
+This project was inspired by the need for a secure password management solution.
+
+## Contact
+
+For any questions or inquiries, please contact [tomhot246@gmail.com](mailto:tomhot246@gmail.com).
